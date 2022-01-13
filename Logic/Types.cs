675a621fbb9276e733065053a679e2ba3584e1c2
Jonathan Allen <jallen@snow.edu>
2022-01-12 20:51:40 -0700

    add basic types and start a specflow feature file

---
+++ Logic/Types.cs
@@ -0,0 +1,26 @@
+﻿namespace Logic;
+public class Assignment
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Group> Groups { get; set; }
+}
+
+public class Group
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Membership> Members { get; set; }
+}
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Membership
+{
+    public Group Group { get; set; }
+    public Student Student { get; set; }
+}
